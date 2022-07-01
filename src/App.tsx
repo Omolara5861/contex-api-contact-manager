046e93254252157830afeca79c0a9af8d29e0757
Omolara5861 <debbiegterra@gmail.com>
2022-07-01 10:30:37 +0100

    expressions in react

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'React';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-          Hello React!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello {name}</h1>
+      <h4>4 + 4 = {4 + 4}</h4>
     </div>
   );
 }
