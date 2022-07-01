0aaf664a839ff13aa23091d7a521c67f96ac9e68
Omolara5861 <debbiegterra@gmail.com>
2022-07-01 10:48:02 +0100

    react class component

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,9 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const name = 'React';
-  const showTitle = true;
-  const showMath = true;
-  let math;
-
-  if (showMath) {
-    math = 50 + 60;
-  }
-  else {
-    math = null;
-  }
   return (
     <div className="App">
-      {showTitle ? <h1>Hello {name}</h1> : null}
-      <h4>{math}</h4>
+     
     </div>
   );
 }
