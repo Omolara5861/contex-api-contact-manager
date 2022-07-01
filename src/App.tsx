35e6b2e4da08e92e5ce8b1a5bf25b00d0f569a82
Omolara5861 <debbiegterra@gmail.com>
2022-07-01 10:52:43 +0100

    rendering component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-     
+     <h1>Contact Manager</h1>
+     <Contact />
     </div>
   );
 }
