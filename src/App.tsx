a008fd1dbaf01950d4f1b639ab6750fd10fb3eb8
Omolara5861 <debbiegterra@gmail.com>
2022-07-01 10:37:50 +0100

    conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const name = 'React';
+  const showTitle = true;
+  const showMath = true;
+  let math;
+
+  if (showMath) {
+    math = 50 + 60;
+  }
+  else {
+    math = null;
+  }
   return (
     <div className="App">
-      <h1>Hello {name}</h1>
-      <h4>4 + 4 = {4 + 4}</h4>
+      {showTitle ? <h1>Hello {name}</h1> : null}
+      <h4>{math}</h4>
     </div>
   );
 }
