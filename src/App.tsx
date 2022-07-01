5a81ef28098fde1bf8a72285ebb7e4817b4775b7
Omolara5861 <debbiegterra@gmail.com>
2022-07-01 11:06:34 +0100

    added header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Contact from './components/Contact';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <div className="App">
-     <h1>Contact Manager</h1>
+     <Header />
      <Contact />
     </div>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Header() {
+  return (
+    <div>
+            <h1>Contact Manager</h1>
+    </div>
+  )
+}
