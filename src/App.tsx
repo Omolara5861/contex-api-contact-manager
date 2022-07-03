68f742ee6e58a8ded0c3192851c218d6864922b3
Omolara5861 <debbiegterra@gmail.com>
2022-07-03 13:17:04 +0100

    added bootstrap styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import './App.css';
 import Contact from './components/Contact';
 import  {Header} from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
      <Header branding="Contact Manager"/>
+     <div className="container">
      <Contact name="John Doe" email="john@gmail.com" phone="555-555-5555"/>
      <Contact name="Karen Doe" email="karen@gmail.com" phone="777-777-7777"/>
+     </div>
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,26 @@ import PropTypes from 'prop-types'
 export const Header =  (props: any) => {
   const { branding} = props;
   return (
-    <div>
-      <h1 style={headingStyle}>{branding}</h1>
+   <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+    <div className="container">
+      <a href="/" className="navbar-brand">{branding}
+      </a>
+      <div>
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <a href="/" className="nav-link">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="/" className="nav-link">
+              Home
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
+   </nav>
   );
 } 
 // Default Props
