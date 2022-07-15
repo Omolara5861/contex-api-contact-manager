7ffb7192d90469874d15d350fbd9ad31ae9b2a04
Omolara5861 <debbiegterra@gmail.com>
2022-07-15 03:07:33 +0100

    styled form

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import  {Header} from './components/layout/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Contacts from './components/contacts/Contacts';
 import { Provider } from './context';
+import AddContact from './components/contacts/AddContact';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
      <Header branding="Contact Manager"/>
      <div className="container">
+     <AddContact/>
      <Contacts />
      </div>
     </div>
