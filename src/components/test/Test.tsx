2936c14931aa219be806067585bba0339b1a6356
Omolara5861 <debbiegterra@gmail.com>
2022-07-19 13:20:58 +0100

    rest api using fetch

--- src/components/test/Test.tsx
+++ src/components/test/Test.tsx
@@ -1,23 +1,36 @@
 import React, { Component } from 'react'
 
 export default class Test extends Component {
-    //Life Cycle Methods
 
+  state = {
+    title: '',
+    body: ''
+  }
+
+    //Life Cycle Methods
     //Method runs after the component output has been rendered to the DOM.
     componentDidMount() {
-        console.log('Component Did Mount');
+      fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then(response => response.json())
+      .then(data => this.setState({ title: data.title, body: data.body }));
     }
     //Method runs before the component output has been rendered to the DOM.
-    componentWillUnmount() {
-        console.log('Component Will Unmount');
-    }
+    // componentWillUnmount() {
+    //     console.log('Component Will Unmount');
+    // }
 
-    componentDidCatch() {
-        console.log('Component Did Catch');
-    }
+    // componentDidCatch() {
+    //     console.log('Component Did Catch');
+    // }
   render() {
+    const { title, body } = this.state;
     return (
-      <div><h1>Test Component</h1></div>
+
+      <div>
+        <h1>Test Component</h1>
+        <h2>{title}</h2>
+        <p>{body}</p>
+      </div>
     )
   }
 }
