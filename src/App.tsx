705dca9331be0fc0224e2068bd0d54bdc2fc2738
Omolara5861 <debbiegterra@gmail.com>
2022-08-29 11:09:00 +0100

    FIXED BUILD BUG

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import  {Header} from './components/layout/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -15,7 +15,7 @@ import AddContact_Ref from './components/contacts/AddContact_Ref';
 function App() {
   return (
     <Provider>
-      <Router>
+      <Router basename='context-api-contact-manager'>
       <div className="App">
      <Header branding="Contact Manager"/>
      <div className="container">
