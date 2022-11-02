7847707970064bdb29cf7f707a45aa917c15759c
Omolara5861 <debbiegterra@gmail.com>
2022-11-02 04:55:39 +0100

    chore: :hammer: Add node engine to package.jsn file

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
-import  {Header} from './components/layout/Header';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Contacts from './components/contacts/Contacts';
-import { Provider } from './context';
-import EditContact from './components/contacts/EditContact';
-import About from './components/pages/About';
-import NotFound from './components/pages/NotFound';
-import Test from './components/test/Test';
-import AddContact_Ref from './components/contacts/AddContact_Ref';
-
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import { Header } from "./components/layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Contacts from "./components/contacts/Contacts";
+import { Provider } from "./context";
+import EditContact from "./components/contacts/EditContact";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
+import Test from "./components/test/Test";
+import AddContact_Ref from "./components/contacts/AddContact_Ref";
 
 function App() {
   return (
     <Provider>
-      <Router basename='context-api-contact-manager'>
-      <div className="App">
-     <Header branding="Contact Manager"/>
-     <div className="container">
-      <Switch>
-        <Route exact path="/" component={Contacts} />
-        <Route path="/contact/add"  component={AddContact_Ref} />
-        <Route path="/contact/edit/:id" component={EditContact} />
-        <Route path="/about" component={About} />
-        <Route path="/test" component={Test} />
-        <Route component={NotFound} />
-      </Switch>
-     </div>
-    </div>
-    </Router>
+      <BrowserRouter basename="context-api-contact-manager">
+        <div className="App">
+          <Header branding="Contact Manager" />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Contacts} />
+              <Route path="/contact/add" component={AddContact_Ref} />
+              <Route path="/contact/edit/:id" component={EditContact} />
+              <Route path="/about" component={About} />
+              <Route path="/test" component={Test} />
+              <Route component={NotFound} />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
     </Provider>
   );
 }
